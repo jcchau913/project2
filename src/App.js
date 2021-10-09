67d1f7d0f7a7c5053e46afbdfe108ffb2146ef3d
Jeff Chau <jchau@us.jvckenwood.com>
2021-10-09 14:40:27 -0700

    covers

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Search from "./components/Search"
 
-export default () => {
+function App () {
   return (
     <div>
       <Search />
     </div>
   )
 }
+
+export default App;
